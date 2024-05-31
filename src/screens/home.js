9382c20ccd5a095ad64dfdabd81b3f6ff2748437
Prafulla-gaikawad <159248731+Prafulla-gaikawad@users.noreply.github.com>
2024-05-31 18:39:51 +0530

    third commit

--- src/screens/home.js
+++ src/screens/home.js
@@ -1,6 +1,8 @@
 import React from "react";
 import Navbar from "../Components/Navbar";
 import Footer from "../Components/Footer";
+import Card from "../Components/Card";
+import Carousal from "../Components/Carousal";
 
 function Home() {
   return (
@@ -9,16 +11,10 @@ function Home() {
         <Navbar></Navbar>
       </div>
       <div>
-        <div className="card" style={{ width: "18rem" }}>
-          <img src="..." className="card-img-top" alt="..." />
-          <div className="card-body">
-            <h5 className="card-title">Card title</h5>
-            <p className="card-text">
-              Some quick example text to build on the card title and make up the
-              bulk of the card's content.
-            </p>
-          </div>
-        </div>
+        <Carousal />
+      </div>
+      <div>
+        <Card />
       </div>
       <div>
         <Footer />
