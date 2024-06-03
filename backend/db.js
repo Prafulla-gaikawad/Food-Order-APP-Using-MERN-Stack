42e870bb4a4cd2f847e087e9f78b681c2e3e7994
Prafulla-gaikawad <159248731+Prafulla-gaikawad@users.noreply.github.com>
2024-06-03 16:55:29 +0530

    sixth commit

--- backend/db.js
+++ backend/db.js
@@ -1,19 +1,21 @@
 const mongoose = require("mongoose");
 const mongoURL =
-  "mongodb+srv://prafulla:mern123@cluster0.25ezqp2.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+  "mongodb+srv://prafulla:mern123@cluster0.25ezqp2.mongodb.net/gofoodmern?retryWrites=true&w=majority&appName=Cluster0";
 
-mongoose.connect(mongoURL);
+const mongoDB = async () => {
+  try {
+    await mongoose.connect(mongoURL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log("connected");
 
-const db = mongoose.connection;
+    const fetched_data = mongoose.connection.db.collection("food_items");
+    const data = await fetched_data.find({}).toArray();
+    console.log();
+  } catch (err) {
+    console.error("--", err);
+  }
+};
 
-db.on("connected", () => {
-  console.log("connected to Mongodb server");
-});
-db.on("error", (err) => {
-  console.error("mongodb connection error", err);
-});
-db.on("disconnected", () => {
-  console.log("mongodb disconnected");
-});
-
-module.exports = db;
+module.exports = mongoDB;
--- backend/index.js
+++ backend/index.js
@@ -2,11 +2,14 @@ const express = require("express");
 const app = express();
 const port = 5000;
 
-const db = require("./db");
+// const db = require("./db");
+const mongoDB = require("./db");
+mongoDB();
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
-
+app.use(express.json());
+app.use("/api", require("./Routes/CreateUser"));
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
