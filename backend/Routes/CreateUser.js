81e05f9077a7d30a4de156d5aaa1105fc3c7e827
Prafulla-gaikawad <159248731+Prafulla-gaikawad@users.noreply.github.com>
2024-06-05 13:12:41 +0530

    eighth commit

--- backend/Routes/CreateUser.js
+++ backend/Routes/CreateUser.js
@@ -2,6 +2,9 @@ const express = require("express");
 const router = express.Router();
 const User = require("../models/User");
 const { body, validationResult } = require("express-validator");
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcryptjs");
+const jwtSecret = "MyNameisPrafullSantoshGaikawad1$#";
 
 router.post(
   "/creatuser",
@@ -15,10 +18,12 @@ router.post(
       return res.status(400).json({ error: errors.array() });
     }
 
+    const salt = await bcrypt.genSalt(10);
+    const secPassword = await bcrypt.hash(req.body.password, salt);
     try {
       await User.create({
         name: req.body.name,
-        password: req.body.password,
+        password: secPassword,
         email: req.body.email,
         location: req.body.location,
       });
@@ -46,10 +51,22 @@ router.post(
       if (!userData) {
         return res.status(400).json({ error: "Login with right credentials" });
       }
-      if (req.body.password !== userData.password) {
+
+      const pwdCompare = await bcrypt.compare(
+        req.body.password,
+        userData.password
+      );
+      if (!pwdCompare) {
         return res.status(400).json({ error: "Login with right credentials" });
       }
-      return res.json({ success: true });
+
+      const data = {
+        user: {
+          id: userData.id,
+        },
+      };
+      const authToken = jwt.sign(data, jwtSecret);
+      return res.json({ success: true, authToken: authToken });
     } catch (error) {
       console.log(error);
       res.json({ success: false });
